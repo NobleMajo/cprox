095aae90f92b42783c29cc836e1f2f325d479374
majo418 <majo@coreunit.net>
2022-02-11 19:05:30 +0100

    Fix: start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+docker network create codec
+
 docker run -d \
     --restart unless-stopped \
     --name cprox \
     -p 8080:8080 \
     -e "VERBOSE=true" \
-    -e "ORIGIN_HOST_PREFIX=coreunit.net"
+    -e "ORIGIN_HOST_PREFIX=coreunit.net" \
     -e "CONTAINER_NAME_PREFFIX=codec_" \
-    --network cprox \
+    --network codec \
     cprox
