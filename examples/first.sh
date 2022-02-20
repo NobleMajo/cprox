0f62ef01f5381bd06088bc5cc74503cd13d07eee
HalsMaulMajo <HalsMaulMajo@coreunit.net>
2022-02-20 15:02:40 +0100

    Change: cert scripts

--- examples/first.sh
+++ examples/first.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
-rm -rf $(pwd)/certs/coreunit.net
+# check if the first argument is set
+if [ -z "$1" ]; then
+    echo "Usage: $0 <CLOUDFLARE_API_TOKEN>"
+    exit 1
+fi
 
-echo "dns_cloudflare_api_token = $API_TOKEN" >> ./cloudflare.ini
+# check if the first argument is a minimum 20 character cloudflare api token
+if [ ${#1} -lt 20 ]; then
+    echo "Usage: $0 <CLOUDFLARE_API_TOKEN>"
+    exit 1
+fi
+
+CLOUDFLARE_API_TOKEN=$1
+echo "dns_cloudflare_api_token = $CLOUDFLARE_API_TOKEN" >> ./cloudflare.ini
 chmod 770 ./cloudflare.ini
 
 docker run -it --rm \
     --name certbot \
     -p 80:80 \
     -v "$(pwd)/cloudflare.ini:/tmp/cloudflare.ini" \
-    -v "$(pwd)/certs:/etc/letsencrypt/archive" \
+    -v "$HOME/certs:/etc/letsencrypt/archive" \
     certbot/dns-cloudflare \
         certonly \
         --email admin@coreunit.net \
         --dns-cloudflare \
         --dns-cloudflare-credentials /tmp/cloudflare.ini \
-        --dns-cloudflare-propagation-seconds 15 \
+        --dns-cloudflare-propagation-seconds 20 \
         --agree-tos \
         --no-eff-email \
         -d coreunit.net \
