74b12878b0053e49d76f5920e5990cefbe845787
majo418 <majo@coreunit.net>
2022-06-25 04:04:00 +0000

    Fix: remote test paths

--- src/test/e2e/env.test.ts
+++ src/test/e2e/env.test.ts
@@ -61,7 +61,10 @@ describe('Live env', function () {
             throw err
         }
         expect(
-            uniqueStringify(env),
+            uniqueStringify({
+                ...env,
+                CERT_PATH: "?" + env.CERT_PATH.substring(env.CERT_PATH.lastIndexOf("cprox"))
+            }),
             "Actual env was: " + JSON.stringify(env, null, 2) + "\n" + out
         ).is.equals(uniqueStringify({
             "PRODUCTION": false,
@@ -78,7 +81,7 @@ describe('Live env', function () {
             "HTTP_PORT": 80,
             "HTTPS_PORT": 443,
             "BIND_ADDRESS": "0.0.0.0",
-            "CERT_PATH": "/home/codec/ws/main/npm/cprox/certs",
+            "CERT_PATH": "?cprox/certs",
             "CERT_NAME": "cert.pem",
             "KEY_NAME": "privkey.pem",
             "CA_NAME": "chain.pem",
